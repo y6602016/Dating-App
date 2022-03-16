332fdeb082b8d4d60eb6023698e7d9d8f2ee213c
洪啟睿 <y6602016@gmail.com>
2022-03-16 10:55:45 -0400

    add OnInit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  user: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(res => {
+      this.user = res;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
