01419a70f52d78545153fa964799ac49124b5ccb
洪啟睿 <y6602016@gmail.com>
2022-03-20 10:52:00 -0400

    add user password

--- API/Entities/Appuser.cs
+++ API/Entities/Appuser.cs
@@ -4,5 +4,9 @@ namespace API.Entities
   {
     public int Id { get; set; }
     public string UserName { get; set; }
+
+    public byte[] Passwordhash { get; set; }
+
+    public byte[] PasswordSalt { get; set; }
   }
 }
