ec7479a8cec0fd50324baa5b9e2f928990cdc07b
洪啟睿 <y6602016@gmail.com>
2022-04-01 20:26:37 -0400

    add userDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,14 +12,16 @@ namespace API.Controllers
   public class AccountController : BaseApiController
   {
     private readonly DataContext _context;
-    public AccountController(DataContext context)
+    private readonly ITokenService _tokenService;
+    public AccountController(DataContext context, ITokenService tokenService)
     {
+      _tokenService = tokenService;
       _context = context;
     }
 
 
     [HttpPost("register")]
-    public async Task<ActionResult<Appuser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
       if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -33,11 +36,15 @@ namespace API.Controllers
       _context.Users.Add(user);
       await _context.SaveChangesAsync();
 
-      return user;
+      return new UserDto
+      {
+        UserName = user.UserName,
+        Token = _tokenService.CreateToken(user)
+      };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<Appuser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
       var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
@@ -57,7 +64,11 @@ namespace API.Controllers
         }
       }
 
-      return user;
+      return new UserDto
+      {
+        UserName = user.UserName,
+        Token = _tokenService.CreateToken(user)
+      };
     }
 
     private async Task<bool> UserExists(string username)
