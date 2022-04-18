eb18b10c94f09bf2d67fa06f227ff10f32f79b33
洪啟睿 <y6602016@gmail.com>
2022-04-18 13:19:47 -0400

    add error middleware

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+
 namespace API.Middleware
 {
   public class ExceptionMiddleware
@@ -12,7 +16,7 @@ namespace API.Middleware
       _next = next;
     }
 
-    public async Task InvolkAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context)
     {
       try
       {
@@ -21,6 +25,19 @@ namespace API.Middleware
       catch (Exception ex)
       {
         _logger.LogError(ex, ex.Message);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+
+        var response = _env.IsDevelopment()
+            ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+            : new ApiException(context.Response.StatusCode, "Internal Server Error");
+
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+        var json = JsonSerializer.Serialize(response, options);
+
+        await context.Response.WriteAsync(json);
       }
     }
   }
