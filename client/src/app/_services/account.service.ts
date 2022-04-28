36a07df26f049ed7a8ba3d2b8f58ec549de6498f
洪啟睿 <y6602016@gmail.com>
2022-04-28 17:49:30 -0400

    add interfaces and member service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 // We usually using service to make http requests
@@ -9,7 +10,7 @@ import { User } from '../_models/user';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = "https://localhost:5001/api/";
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
@@ -31,7 +32,7 @@ export class AccountService {
   register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
-        if (user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
@@ -45,7 +46,7 @@ export class AccountService {
   }
 
   logout() {
-   localStorage.removeItem('user')
-   this.currentUserSource.next(null);
+    localStorage.removeItem('user')
+    this.currentUserSource.next(null);
   }
 }
