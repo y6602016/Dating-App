edbf928c57cf54320307c113206bdd499b5dfd67
洪啟睿 <y6602016@gmail.com>
2022-05-04 15:36:39 -0400

    routing to detail page

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -20,6 +20,6 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + "user/" + username);
+    return this.http.get<Member>(this.baseUrl + "users/" + username);
   }
 }
